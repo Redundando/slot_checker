ccf59ed0e2da6ca674fdfec7845c0b7b93a02f0a
Arved <info@primedope.com>
2023-06-20 12:48:55 +0200

    Slugs can now be loaded via xlsx

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import json
 import os.path
 import urllib.request
-from bs4 import BeautifulSoup
-import pandas as pd
-from slugs import slugs
 
-#slugs = ["moonlight-fortune-", "cool-jewels", "wild-bison-charge"]
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
 def check_game(slug, language="EN"):
@@ -23,30 +21,40 @@ def check_game(slug, language="EN"):
         result["src"] = iframe["src"]
         return result
 
-def check_slugs(slugs, language="EN", re_check_previously_checked_games = False):
+
+def check_slugs(slugs, language="EN", re_check_previously_checked_games=False):
     result = []
     if os.path.exists("games.json"):
         with open('games.json', 'r') as file:
             result = json.load(file)
-    for slug in slugs:
+    for slug in slugs[:5]:
         try:
-            processed_slugs = [r["slug"]+"/"+r["language"] for r in result]
-            if (slug+"/"+language in processed_slugs):
-
+            processed_slugs = [r["slug"] + "/" + r["language"] for r in result]
+            if (slug + "/" + language in processed_slugs):
                 if not re_check_previously_checked_games:
+                    print(f"Game {slug}, language {language} already checked. Not re-checking.")
                     continue
-                result = [r for r in result if (result["slug"] != slug and result["language"]!=language)]
+                result = [r for r in result if (r["slug"] != slug and r["language"] != language)]
 
-            result.append(check_game(slug,language=language))
+            result.append(check_game(slug, language=language))
             with open("games.json", "w") as file:
-                file.write(json.dumps(result,indent=4))
+                file.write(json.dumps(result, indent=4))
         except Exception as e:
             print(e)
+
 
 def create_xls(filename="games.json"):
     print("Creating Excel File")
     pd.read_json(filename).to_excel("games.xlsx")
 
-for l in ["EN"]:
-    check_slugs(slugs, language=l)
-create_xls()
+
+def load_slugs(filename="slugs.xlsx"):
+    df = pd.read_excel('slugs.xlsx')
+    return df.iloc[:, 0].tolist()
+
+
+if __name__ == "__main__":
+    slugs = load_slugs()
+    for language in ["EN"]:
+        check_slugs(slugs, language=language, re_check_previously_checked_games=False)
+    create_xls()
